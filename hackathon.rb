34413d1c428b463542e0bb10edcecc4592d937ae
Shicholas <nicholas.shook@gmail.com>
2013-04-11 19:34:40 -0700

    version bump for asset gems

--- hackathon.rb
+++ hackathon.rb
@@ -23,10 +23,10 @@ gem 'sqlite3'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails',   '4.0.0.beta1'
+  gem 'coffee-rails', '4.0.0.beta1'
   gem "bootstrap-sass"
-  gem 'uglifier', '>= 1.0.3'
+  gem 'uglifier'
 end
 
 gem 'jquery-rails'
